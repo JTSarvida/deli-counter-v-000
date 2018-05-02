b56b8afee7557ec60e34a743431395252545192d
Joshua Tan <jtsarvida@gmail.com>
2018-05-02 11:15:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+
+def line(katz_deli)
+  index = 0
+  if katz_deli != []
+    katz_deli.each do |people|
+      puts "The line is currently: #{index + 1}. #{people}"
+    end
+  else
+    puts "The line is currently empty."
+  end
+end
