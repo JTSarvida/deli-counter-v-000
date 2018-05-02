9d6576ad70d7bde4e0fbf41ffcd708eb3c617e6b
Joshua Tan <jtsarvida@gmail.com>
2018-05-02 11:26:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@
 def line(katz_deli)
   index = 0
   if katz_deli != []
-    katz_deli.each |people|
+    katz_deli.each while index < katz_deli.length |people|
       puts "The line is currently: #{index + 1}. #{people}"
       index += 1
     end
